34d0feab049e86fe876b2b44722ad816a6c89961
Alliali <45864770+Alliali@users.noreply.github.com>
2021-11-16 23:59:49 +0900

    Revert "Merge branch 'main' of https://github.com/Alliali/2DGP"
    This reverts commit 7b2ea72ab8993ff09b1a375b013ce6c5b2f6dd77, reversing
    changes made to b47f422f533491a2beb2ffd544ef24c26e1e40c2.

--- Labs/Lecture11_Character_Controller/boy.py
+++ Labs/Lecture11_Character_Controller/boy.py
@@ -1,8 +1,6 @@
 from pico2d import *
 
 # Boy Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP = range(4)
-
 # fill here
 
 key_event_table = {
@@ -15,91 +13,16 @@ key_event_table = {
 
 
 # Boy States
-class IdleState:
-    def enter(boy, event):
-        if event == RIGHT_DOWN:
-            boy.velocity += 1
-        elif event == LEFT_DOWN:
-            boy.velocity -= 1
-        elif event == RIGHT_UP:
-            boy.velocity -= 1
-        elif event == LEFT_UP:
-            boy.velocity += 1
-        boy.timer = 1000
 
-    def exit(boy, event):
-        pass
-
-    def do(boy):
-        boy.frame = (boy.frame + 1) % 8
-        boy.timer -= 1
-
-    def draw(boy):
-        if boy.dir == 1:
-            boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
-        else:
-            boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
-
-class RunState:
-    def enter(boy, event):
-        if event == RIGHT_DOWN:
-            boy.velocity += 1
-        elif event == LEFT_DOWN:
-            boy.velocity -= 1
-        elif event == RIGHT_UP:
-            boy.velocity -= 1
-        elif event == LEFT_UP:
-            boy.velocity += 1
-        boy.dir = boy.velocity
-
-    def exit(boy, event):
-        pass
-
-    def do(boy):
-        boy.frame = (boy.frame + 1) % 8
-        boy.timer -= 1
-        boy.x += boy.velocity
-        boy.x = clamp(25, boy.x, 800 - 25)
-
-    def draw(boy):
-        if boy.velocity == 1:
-            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
-        else:
-            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
 # fill here
 
 next_state_table = {
-    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState}
+# fill here
 }
 
 
 
-class RunState:
-    def enter(boy, event):
-        if event == RIGHT_DOWN:
-            boy.velocity += 1
-        elif event == LEFT_DOWN:
-            boy.velocity -= 1
-        elif event == RIGHT_UP:
-            boy.velocity -= 1
-        elif event == LEFT_UP:
-            boy.velocity += 1
-        boy.dir = boy.velocity
 
-    def exit(boy, event):
-        pass
-
-    def do(boy):
-        boy.frame = (boy.frame + 1) % 8
-        boy.timer -= 1
-        boy.x += boy.velocity
-        boy.x = clamp(25, boy.x, 800 - 25)
-
-    def draw(boy):
-        if boy.velocity == 1:
-            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
-        else:
-            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
 
 
 
